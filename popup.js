4a2f1a03141ce9fe531badb12d5de381fedf94a9
tanya290498 <29tatyana04@gmail.com>
2019-09-09 17:11:03 +0300

    filarumpl project has been updated

--- popup.js
+++ popup.js
@@ -1,29 +1,44 @@
-setInterval(() => {
-    document.addEventListener('mousemove', function (e) {
-        if (e.clientY === 70 || e.clientY < 70) {
+let count = 0;
+let xhr = new XMLHttpRequest();
+
+document.addEventListener('mousemove', function (event) {
+    let timerId = setTimeout(() => {
+        if (event.clientY === 20 || event.clientY < 20) {
             show();
         }
-        // console.log(e.clientY);
-    });
-}, 1000);
+    }, 400);
+});
 
 
 function show() {
-    var popup = document.getElementById('popup');
+    let popup = document.getElementById('popup');
+    // let form = document.getElementById('form');
     popup.style.display = 'block';
 }
 
 function hide() {
-    var popup = document.getElementById('popup');
+    let popup = document.getElementById('popup');
     popup.style.display = 'none';
-    console.log('hello');
 }
 
-function isNumber(e) {
-    var iKeyCode = e.keyCode;
-    if (iKeyCode != 46 && iKeyCode > 31 && (iKeyCode < 48 || iKeyCode > 57))
-        return false;
+function isNumber(event) {
+    let keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
+    keys.forEach(key => {
+        if (event.key !== key) {
+            return false;
+        }
         return true;
-    
+    });
+
+    if (event.keyCode !== undefined && event.which) {
+        let code = event.keyCode || event.which;
+        if (code !== 46 && code > 31 && (code < 48 || code > 57)) {
+            return false;
+        }
+        return true;
+    }
 }
+
+// $('#register_mobile,#register_phone').mask("999999999", {placeholder: "_________"});
+

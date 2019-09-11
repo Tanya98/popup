e0971e028efb23e50f40a7adac9f56454f86a972
tanya290498 <29tatyana04@gmail.com>
2019-09-11 18:00:26 +0300

    some fixes have been done

--- popup.js
+++ popup.js
@@ -1,7 +1,7 @@
 function CookieService() {
     this.setCookie = function (key, value) {
         document.cookie = key + "=" + value;
-    }
+    };
 
     this.getCookie = function (key) {
         let matches = document.cookie.match(new RegExp(
@@ -12,17 +12,19 @@ function CookieService() {
 }
 
 function HttpService() {
-    this.post = function (url, body, callback) {
+    this.post = function (url, bodty, callback) {
 
         let xhr = new XMLHttpRequest();
+        let response;
 
         xhr.open('POST', url, true);
         xhr.setRequestHeader('Content-type', 'application/json; charset=utf-8');
         xhr.send(JSON.stringify(body));
 
         xhr.onreadystatechange = function () {
-            if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-                callback(xhr.responseText)
+            if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+                response = JSON.parse(xhr.responseText);
+                callback(response);
             } else {
                 throw xhr.responseText;
             }
@@ -34,82 +36,104 @@ function Popup(elementId) {
 
     this.show = function () {
         let popup = document.getElementById(elementId);
-        let body = document.body;
-        popup.style.display = 'block';
+        // let body = document.body;
+        $(popup).show();
+        // popup.style.display = 'block';
         // body.style.overflowY = 'hidden';
-    }
+    };
 
     this.hide = function () {
         let popup = document.getElementById(elementId);
-        popup.style.display = 'none';
-    }
+        $(popup).hide();
+        // popup.style.display = 'none';
+    };
 
-    this.submitEventHandler = function () { }
+    this.submitEventHandler = function () {
+    };
 }
 
 const popup = new Popup('popup');
+let newCookie = new CookieService();
+
 let popupCounter = 0;
+
+let userRegistered = false;
+
+let widthBody = document.body.style.width;
+let clientExists = document.getElementById('clientExists');
+let phoneNumberSaved = document.getElementById('phoneNumberSaved');
+let phoneNumber = document.getElementById('phone_number');
+
 
 $(document).ready(function () {
 
-    let newCookie = new CookieService();
-
     newCookie.setCookie('isNew', 'true');
-    let isNewCookie = Boolean(newCookie.getCookie('isNew'));
 
-    let phoneNumber = document.getElementById('phone_number');
+    let isNewUser = Boolean(newCookie.getCookie('isNew'));
 
     $(phoneNumber).mask('999999999');
 
-    let resultMessage1 = document.getElementById('message-1');
-    let resultMessage2 = document.getElementById('message-2');
-
-    if (popup.hide()) {
-        newCookie.setCookie('isNew', 'false');
-        let isNewCookie = Boolean(newCookie.getCookie('isNew'));
-        alert(isNewCookie);
-    }
-
     popup.submitEventHandler = function () {
+        // if (cookie.getCookie('isNew'))
+        sendPhoneNumber(phoneNumber.value, function (response) {
+            if (response.EntryStatus === 1 || response.EntryStatus === 2 || response.EntryStatus === 4) {
+                phoneNumberSaved.style.display = 'block';
+            }
+            if (response.EntryStatus === 3) {
+                clientExists.style.display = 'block';
+            }
+        });
+    };
 
-        if (cookie.getCookie('isNew'))
-            sendPhoneNumber(phoneNumber.value, function (response) {
-                let statuses = Object.keys(response.AntiAbandonEntryStatus);
-                statuses.forEach(status => {
-                    if (status === 'InProcess' || status === 'Removed') {
-                        resultMessage2.style.display = 'block';
-                    } else {
-                        resultMessage1.style.display = 'block';
-                    }
-                });
-            });
-    }
-
-    document.addEventListener('mousemove', function (event) {
+    document.onmousemove = function (event) {
         if (event.clientY === 20 || event.clientY < 20) {
-            if (isNewCookie === true && popupCounter < 1) {
+            userRegistered = true;
+            if (popupCounter < 1) {
                 setTimeout(() => {
                     popup.show();
-                }, 300);
+                }, 200);
                 popupCounter++;
             }
         }
-    });
+    };
+
+    if (widthBody <= 414 + 'px') {
+        userRegistered = true;
+        setTimeout(() => {
+            popup.show();
+        }, 5000);
+    }
+
+    document.onkeydown = function (event) {
+        if (event.key === 'Escape') {
+            popup.hide();
+        }
+    };
 });
+
+// function showPopUp() {
+//     // debugger;
+//
+// }
+
+// function showPopUpForMobile() {
+//     // debugger;
+//
+// }
 
 function sendPhoneNumber(phoneNumber, callback) {
     let requestBody = {
         AuthInfo: {
             AccessKey: 'c9b64244-2dc8-44db-89a5-c6ae72997bbc',
-            ServiceUrl: 'http://dev-by-9/ecs_long/api/CustomerModule/AddPhoneNumberToAntiAbandonTool'
+            ServiceUrl: ''
         },
         phoneNumber: phoneNumber
     };
 
     let http = new HttpService();
 
-    http.post(info.AuthInfo.ServiceUrl, requestBody, callback);
-};
+    http.post('http://dev-by-9/ecs_long/api/CustomerModule/AddPhoneNumberToAntiAbandonTool', requestBody, callback);
+}
 
 function isNumber(event) {
     if (event.keyCode || event.which) {
